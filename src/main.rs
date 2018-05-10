fdc263d7b6bcd7115ebea1e1169964ae5332c183
userFromHades <userfromhades@gmail.com>
2018-05-11 01:43:22 +0300

    space to tab

--- src/main.rs
+++ src/main.rs
@@ -12,39 +12,39 @@ mod mesh;
 
 fn main() {
 
-    let teaport = wavefront_obj::load_from_file("./teapot.obj");
-    let head = wavefront_obj::load_from_file("./african_head.obj");
+	let teaport = wavefront_obj::load_from_file("./teapot.obj");
+	let head = wavefront_obj::load_from_file("./african_head.obj");
 
-    println!("Hello, world!");
-    //canvas::test();
-    let mut c = canvas::MyCanvas::new (800, 600);
-    //c.test();
+	println!("Hello, world!");
+	//canvas::test();
+	let mut c = canvas::MyCanvas::new (800, 600);
+	//c.test();
 
-    c.clear();
-    c.point(128, 128, 0xff0000);
+	c.clear();
+	c.point(128, 128, 0xff0000);
 
-    c.line(  10,  10, 200,  20, 0xff0000);
-    c.line( 10,  10,  20, 200, 0x00ff00);
+	c.line(  10,  10, 200,  20, 0xff0000);
+	c.line( 10,  10,  20, 200, 0x00ff00);
 
-    c.line( 210,  210,  20, 200, 0xffff00);
-    c.line( 210,  210,  200, 20, 0xff0000);
+	c.line( 210,  210,  20, 200, 0xffff00);
+	c.line( 210,  210,  200, 20, 0xff0000);
 
-    //c.line( 10,  10,  210, 210, 0xff0000);
-    c.line( 210, 210, 10,  10,  0xff0000);
-    //c.line( 200, 20, 20,  200,  0xff00ff);
-    c.line( 20,  200, 200, 20,   0xff00ff);
+	//c.line( 10,  10,  210, 210, 0xff0000);
+	c.line( 210, 210, 10,  10,  0xff0000);
+	//c.line( 200, 20, 20,  200,  0xff00ff);
+	c.line( 20,  200, 200, 20,   0xff00ff);
 
-    //c.daraw_triangle_list(vertex, index);
+	//c.daraw_triangle_list(vertex, index);
 
-    c.draw_solid_triangle(
-        vec3::Vec3::new(0.5, 0.5, 0.0),
-        vec3::Vec3::new(0.0, 0.5, 0.0),
-        vec3::Vec3::new(0.5, 0.0, 5.5), 0xffffff);
+	c.draw_solid_triangle(
+	    vec3::Vec3::new(0.5, 0.5, 0.0),
+	    vec3::Vec3::new(0.0, 0.5, 0.0),
+	    vec3::Vec3::new(0.5, 0.0, 5.5), 0xffffff);
 
-    teaport.draw(&mut c, 0.2, vec3::Vec3::new(0.0, -0.5, 1.0));
-    teaport.draw(&mut c, 0.2, vec3::Vec3::new(0.3, -0.2, 1.5));
-    head.draw   (&mut c, 0.5, vec3::Vec3::new(-0.5, 0.0, 0.5));
+	teaport.draw(&mut c, 0.2, vec3::Vec3::new(0.0, -0.5, 1.0));
+	teaport.draw(&mut c, 0.2, vec3::Vec3::new(0.3, -0.2, 1.5));
+	head.draw   (&mut c, 0.5, vec3::Vec3::new(-0.5, 0.0, 0.5));
 
-    c.wait_end();
+	c.wait_end();
 
 }
--- src/mesh.rs
+++ src/mesh.rs
@@ -5,59 +5,59 @@ use vec3;
 
 #[allow(dead_code)]
 fn clump (x : f32, a :f32, b: f32) -> f32 {
-    if x < a{
-        a
-    }else if x > b {
-        b
-    }
-    else {x}
+	if x < a{
+		a
+	}else if x > b {
+		b
+	}
+	else {x}
 }
 
 
 
 bitflags! {
-    pub struct VertexType: u32 {
-        const POSITION = 0b00000001;
-        const TEXTURE  = 0b00000010;
-        const NORMALE  = 0b00000100;
-     }
+	pub struct VertexType: u32 {
+		const POSITION = 0b00000001;
+		const TEXTURE  = 0b00000010;
+		const NORMALE  = 0b00000100;
+	}
 }
 
 impl VertexType {
-    pub fn size (self) -> usize {
-        let mut size : usize = 0;
-        if ! (self &  VertexType::POSITION).is_empty() {
-            size += 3 * 4;
-        }
-        if ! (self & VertexType::TEXTURE).is_empty() {
-            size += 2 * 4;
-        }
-        if ! (self & VertexType::NORMALE).is_empty() {
-            size += 3 * 4;
-        }
-        size
-    }
+	pub fn size (self) -> usize {
+		let mut size : usize = 0;
+		if ! (self &  VertexType::POSITION).is_empty() {
+			size += 3 * 4;
+		}
+		if ! (self & VertexType::TEXTURE).is_empty() {
+			size += 2 * 4;
+		}
+		if ! (self & VertexType::NORMALE).is_empty() {
+			size += 3 * 4;
+		}
+		size
+	}
 
-    pub fn count (self) -> usize {
-        let mut size : usize = 0;
-        if ! (self &  VertexType::POSITION).is_empty() {
-            size += 3;
-        }
-        if ! (self & VertexType::TEXTURE).is_empty() {
-            size += 2;
-        }
-        if ! (self & VertexType::NORMALE).is_empty() {
-            size += 3;
-        }
-        size
-    }
-}
+	pub fn count (self) -> usize {
+		let mut size : usize = 0;
+		if ! (self &  VertexType::POSITION).is_empty() {
+			size += 3;
+		}
+		if ! (self & VertexType::TEXTURE).is_empty() {
+			size += 2;
+		}
+		if ! (self & VertexType::NORMALE).is_empty() {
+			size += 3;
+		}
+		size
+	}
+	}
 
 pub struct Mesh {
-    vertex  : Vec<f32>,
-    index   : Vec<u32>,
-    
-    vertex_type : VertexType
+	vertex  : Vec<f32>,
+	index   : Vec<u32>,
+	
+	vertex_type : VertexType
 }
 
 impl Mesh {
@@ -73,21 +73,21 @@ impl Mesh {
 		                 self.vertex[(index * count + 2) as usize])
 	}
 
-    pub fn draw (& self,
-                 c : &mut canvas::MyCanvas, 
-                 scale : f32,
-                 offset : vec3::Vec3) 
-    {
-        let index  = & self.index;
-        let vertex = & self.vertex;
+	pub fn draw (& self,
+	             c : &mut canvas::MyCanvas, 
+	             scale : f32,
+	             offset : vec3::Vec3) 
+	{
+		let index  = & self.index;
+		let vertex = & self.vertex;
 
-        let count = self.vertex_type.count();
+		let count = self.vertex_type.count();
 
-        for i in 0..index.len()/3 {
+		for i in 0..index.len()/3 {
 
-            let idx0 = (index[i * 3 + 0]) as usize;
-            let idx1 = (index[i * 3 + 1]) as usize;
-            let idx2 = (index[i * 3 + 2]) as usize;
+			let idx0 = (index[i * 3 + 0]) as usize;
+			let idx1 = (index[i * 3 + 1]) as usize;
+			let idx2 = (index[i * 3 + 2]) as usize;
 
 			let p0 = &self.get_position(idx0).scaled(scale) + &offset;
 			let p1 = &self.get_position(idx1).scaled(scale) + &offset;
@@ -97,18 +97,18 @@ impl Mesh {
 			let v2 = &p2 - &p0;
 			let n = vec3::cross_product (v1, v2).normalized();
 
-            if n.z >= 0.0 {
-                continue;
-            }
+			if n.z >= 0.0 {
+				continue;
+			}
 
-            let l = vec3::Vec3::new(1.0, 0.0, 0.0).normalized();
+			let l = vec3::Vec3::new(1.0, 0.0, 0.0).normalized();
 
-            let f = clump (0.5 + 0.5 * vec3::dot_product(n, l), 0.0, 1.0);
+			let f = clump (0.5 + 0.5 * vec3::dot_product(n, l), 0.0, 1.0);
 
-            let cl = (f * 255.0) as u32;
-            let color : u32 = (cl << 16) | (cl << 8) | cl;
+			let cl = (f * 255.0) as u32;
+			let color : u32 = (cl << 16) | (cl << 8) | cl;
 
 			c.draw_solid_triangle (p0, p1, p2, color);
-        }
-    }
+		}
+	}
 }
